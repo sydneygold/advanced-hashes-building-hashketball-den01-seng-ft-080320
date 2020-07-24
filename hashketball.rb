02f2353ebcf252661a4708ccd0884128d11b5f4a
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-24 18:47:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,16 @@ game_hash = {
     :players => {
     :player_name => "Alan Anderson",
     :number => 0,
-    :shoe = 16,
-    :points = 22,
-    :rebounds = 12,
-    :assists = 12,
-    :steals = 3,
-    :blocks = 1,
-    :slam_dunks => 
+    :shoe => 16,
+    :points => 22,
+    :rebounds => 12,
+    :assists => 12,
+    :steals => 3,
+    :blocks => 1,
+    :slam_dunks => 1
       },
     {
-    :player_name = ,
+    :player_name => "" ,
     :number = ,
     :shoe = ,
     :points = ,
