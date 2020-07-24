f7b4a1a0cd17d9c7497faf8a545ee0e016d3e5c8
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-24 18:34:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,13 +2,17 @@ game_hash = {
   :home => { :team_name => "Brooklyn Nets",
     :colors => { "Black", "White" },
     :players => {
-
+    :player_name => {
+      
+      }
     }
   },
   :away => { :team_name => "Charlotte Hornets",
     :color => { "Turquoise", "Purple" },
     :players => {
-      
+    :player_name => {
+
+      }
     }
   }
 }
